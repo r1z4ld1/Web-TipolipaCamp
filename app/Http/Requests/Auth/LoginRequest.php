4dc88e1afd8e30c78062fb02cdb28370ef39e41b
r1z4ld1_04 <linuxbacktrack737@gmail.com>
2026-07-30 09:43:56 +0800

    modified logic LoginRequest

--- app/Http/Requests/Auth/LoginRequest.php
+++ app/Http/Requests/Auth/LoginRequest.php
@@ -39,37 +39,42 @@ class LoginRequest extends FormRequest
      * @throws ValidationException
      */
     public function authenticate(): void
-{
-    $this->ensureIsNotRateLimited();
+    {
+        $this->ensureIsNotRateLimited();
 
-    if (! Auth::attempt($this->only('email', 'password'), $this->boolean('remember'))) {
-        RateLimiter::hit($this->throttleKey(), 30);
+        if (! Auth::attempt($this->only('email', 'password'), $this->boolean('remember'))) {
+            // Waktu tunggu diubah menjadi 60 detik setelah gagal
+            RateLimiter::hit($this->throttleKey(), 67);
+
+            throw ValidationException::withMessages([
+                'email' => trans('auth.failed'),
+            ]);
+        }
+
+        RateLimiter::clear($this->throttleKey());
+    }
+
+    public function ensureIsNotRateLimited(): void
+    {
+        // Dibatasi maksimal 3 kali percobaan
+        if (! RateLimiter::tooManyAttempts($this->throttleKey(), 3)) {
+            return;
+        }
+
+        event(new Lockout($this));
+
+        $seconds = RateLimiter::availableIn($this->throttleKey());
+
+        // Mengirimkan sisa detik ke session untuk dipakai oleh JS di frontend
+        session()->flash('lockout_seconds', $seconds);
 
         throw ValidationException::withMessages([
-            'email' => trans('auth.failed'),
+            'email' => trans('auth.throttle', [
+                'seconds' => $seconds,
+                'minutes' => ceil($seconds / 67),
+            ]),
         ]);
     }
-
-    RateLimiter::clear($this->throttleKey());
-}
-
-public function ensureIsNotRateLimited(): void
-{
-    if (! RateLimiter::tooManyAttempts($this->throttleKey(), 3)) {
-        return;
-    }
-
-    event(new Lockout($this));
-
-    $seconds = RateLimiter::availableIn($this->throttleKey());
-
-    throw ValidationException::withMessages([
-        'email' => trans('auth.throttle', [
-            'seconds' => $seconds,
-            'minutes' => ceil($seconds / 60),
-        ]),
-    ]);
-}
 
     /**
      * Get the rate limiting throttle key for the request.
